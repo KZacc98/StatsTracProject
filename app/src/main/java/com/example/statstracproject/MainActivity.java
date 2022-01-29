6b21854264659a6fdd0e21372c794388d9f03067
Kamil <kamil.zachara98@gmail.com>
2022-01-29 21:09:39 +0100

    dev created cuz forgetti

--- app/src/main/java/com/example/statstracproject/MainActivity.java
+++ app/src/main/java/com/example/statstracproject/MainActivity.java
@@ -79,11 +79,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-//        ArrayList<Contact> contacts = new ArrayList<>();//irl tu wpadają rzeczy z serwera
-//        contacts.add(new Contact("Maddy Perez","madper@wp.pl","https://static.wikia.nocookie.net/euphoria-hbo/images/1/13/Maddy_Perez.jpg/revision/latest?cb=20200821140939.jpg"));
-//        contacts.add(new Contact("Jules Vaughm","juulz@wp.pl","https://static.wikia.nocookie.net/euphoria-hbo/images/0/06/Jules_Vaughn.jpg/revision/latest/scale-to-width-down/1200?cb=20190617063942.jpg"));
-//        contacts.add(new Contact("cassie Howard","cascas@wp.pl","https://static.wikia.nocookie.net/euphoria-hbo/images/f/f9/Euphoria_S2_Cassie.jpg/revision/latest?cb=20220104035238.jpg"));
-//        contacts.add(new Contact("Elliot","ellie@wp.pl","https://static.wikia.nocookie.net/euphoria-hbo/images/7/7d/Euphoria_S2_Elliot.jpg/revision/latest?cb=20211225055956.jpg"));
 
 
 
